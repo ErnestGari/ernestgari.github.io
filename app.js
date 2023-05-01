826d2324e20bd9c3bf25c1e2e201774c05c1b3f9
Ernestas <ernestas.garipovas@gmail.com>
2023-05-01 19:22:57 +0300

    new design changes
    added a circle that follows around the cursor.
    
    change the flavicon of the website
    
    renamed the website
    
    changed highlighting text from default to white and changed text to black

--- app.js
+++ app.js
@@ -14,3 +14,9 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+var cursor = document.getElementById("cursor");
+document.body.addEventListener("mousemove", function(e) {
+  cursor.style.left = e.clientX + "px",
+    cursor.style.top = e.clientY + "px";
+});
